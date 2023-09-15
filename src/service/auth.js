6c85c8fb61d4f347ad1377d9958f0646ea6e3134
Colin Smith <88501518+colinksmith@users.noreply.github.com>
2023-09-15 16:59:18 -0400

    changed response to status codes and deleted unused code

--- src/service/auth.js
+++ src/service/auth.js
@@ -3,69 +3,6 @@ const validator = require("validator");
 const User = require("../models/User");
 const mongoose = require("mongoose")
 
-// exports.getLogin = (req, res) => {
-//   if (req.user) {
-//     return res.redirect("/profile");
-//   }
-//   res.render("login", {
-//     title: "Login",
-//   });
-// };
-
-// exports.postLogin = (req, res, next) => {
-//   const validationErrors = [];
-//   if (!validator.isEmail(req.body.email))
-//     validationErrors.push({ msg: "Please enter a valid email address." });
-//   if (validator.isEmpty(req.body.password))
-//     validationErrors.push({ msg: "Password cannot be blank." });
-
-//   if (validationErrors.length) {
-//     req.flash("errors", validationErrors);
-//     return res.redirect("/login");
-//   }
-//   req.body.email = validator.normalizeEmail(req.body.email, {
-//     gmail_remove_dots: false,
-//   });
-
-//   passport.authenticate("local", (err, user, info) => {
-//     if (err) {
-//       return next(err);
-//     }
-//     if (!user) {
-//       req.flash("errors", info);
-//       return res.redirect("/login");
-//     }
-//     req.logIn(user, (err) => {
-//       if (err) {
-//         return next(err);
-//       }
-//       req.flash("success", { msg: "Success! You are logged in." });
-//       res.redirect(req.session.returnTo || "/profile");
-//     });
-//   })(req, res, next);
-// };
-
-// exports.logout = (req, res) => {
-//   req.logout(() => {
-//     console.log('User has logged out.')
-//   })
-//   req.session.destroy((err) => {
-//     if (err)
-//       console.log("Error : Failed to destroy the session during logout.", err);
-//     req.user = null;
-//     res.redirect("/");
-//   });
-// };
-
-// exports.getSignup = (req, res) => {
-//   if (req.user) {
-//     return res.redirect("/profile");
-//   }
-//   res.render("signup", {
-//     title: "Create Account",
-//   });
-// };
-
 exports.postSignup = async (req, res, next) => {
   console.log(req.body)
   const validationErrors = [];
@@ -74,7 +11,7 @@ exports.postSignup = async (req, res, next) => {
 
   if (validationErrors.length) {
     req.flash("errors", validationErrors);
-    return res.redirect("../signup");
+    return res.status(400).send(validationErrors.map(x => x.msg).join(', '));
   }
   req.body.email = validator.normalizeEmail(req.body.email, {
     gmail_remove_dots: false,
@@ -92,19 +29,9 @@ exports.postSignup = async (req, res, next) => {
     req.flash("errors", {
       msg: "Account with that email address or username already exists.",
     });
-    return //res.redirect("../signup");
+    return res.status(409).send("Account already exists");
   }
+
   await user.save()
-    
-  // (err) => {
-  //   if (err) {
-  //     return next(err);
-  //   }
-  //   req.logIn(user, (err) => {
-  //     if (err) {
-  //       return next(err);
-  //     }
-  //     res.redirect("/profile");
-  //   });
-  // };
+  return res.status(201).send("Account created")  
 };
